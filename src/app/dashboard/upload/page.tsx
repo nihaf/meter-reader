d997fdc86eeff4d103bebef6a7560a6ba27a1b32
Nils Haffke <2759236+nihaf@users.noreply.github.com>
2025-11-17 08:15:39 +0100

    Don't open camera directly on mobile devices on upload

--- src/app/dashboard/upload/page.tsx
+++ src/app/dashboard/upload/page.tsx
@@ -197,7 +197,6 @@ export default function UploadPage() {
                             type="file"
                             className="sr-only"
                             accept="image/*"
-                            capture="environment"
                             onChange={handleFileSelect}
                           />
                         </span>
